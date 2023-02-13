2f3dfa214f7021a871b3787e37b5a9e51392da72
Ivan <pl_st@list.ru>
2023-02-13 12:28:12 +0300

    feat: added other tours section

---
+++ src/components/OtherTours.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tours">
+    <h1 class="tours__title">Other tours</h1>
+    <div class="tours__list">
+      <tour-card
+          v-for="tour in tours"
+          :key="tour.id"
+          :tour="tour"
+          class="tours__list-item"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+  import TourCard from "@/components/TourCard.vue";
+
+  export default {
+    name: "OtherTours",
+    components: {
+      TourCard
+    },
+    data() {
+      return {
+        tours: [
+          {
+            id: 1,
+            title: 'St. Isaac\'s Cathedral in St. Petersburg',
+            caption: 'One of the highest domed structures in the world.',
+            imgName: 'Tour1.png'
+          },
+          {
+            id: 2,
+            title: 'The Bridge of Peace, Tbilisi',
+            caption: 'Arc-shaped pedestrian bridge made of glass and steel',
+            imgName: 'Tour2.png'
+          },
+          {
+            id: 3,
+            title: 'El Caminito del Rey, Argentina',
+            caption: 'Tango, Open-air Museum, Painting, Art and History',
+            imgName: 'Tour3.png'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tours {
+    &__title {
+      margin-bottom: 64px;
+    }
+    &__list {
+      display: flex;
+      justify-content: space-between;
+
+      &-item {
+        width: calc(100% / 3 - 66px/2);
+      }
+    }
+  }
+</style>
---
+++ src/components/TourCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="tour">
+    <img :src="getImgUrl(tour.imgName)" class="tour__image" alt="tour image">
+    <h2 class="tour__title">
+      {{ tour.title }}
+    </h2>
+    <p class="tour__caption">
+      {{ tour.caption }}
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TourCard",
+    props: {
+      tour: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      getImgUrl(path) {
+        return  require('@/assets/images/' + path)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/styles/variables.scss";
+
+  .tour {
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+
+    &__image {
+      width: 100%;
+      margin-bottom: 24px;
+    }
+    &__title {
+      color: $font-primary;
+      font-weight: 500;
+      font-size: 32px;
+      line-height: 38px;
+      margin-bottom: 16px;
+    }
+    &__caption {
+      color: $font-secondary;
+      font-weight: 400;
+      font-size: 16px;
+    }
+  }
+</style>
